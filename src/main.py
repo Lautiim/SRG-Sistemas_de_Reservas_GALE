85b4772d0583d0495abfd32a7d71e4854a35a03a
Lautiim <lasalto05@gmail.com>
2025-09-12 17:43:07 -0300

    Ajuste a main Se creo el "Template" sobre el cual se van a desarrollar las funcionalidades a futuro.

--- src/main.py
+++ src/main.py
@@ -1,20 +1,50 @@
-def registrar_hotel():
-    print("Funcionalidad para registrar hotel (pendiente de implementar)")
-
 def menu():
-    print("1. Registrar Cliente")
-    print("2. Registrar Habitacion")
-    print("3. Registrar Reserva")
-    print("4. Salir")
+    # Muestra las opciones del menú principal
+    print("1. Gestion de Hoteles")
+    print("2. Gestion de Clientes")
+    print("3. Gestion de Reservas")
+    print("4. Gestionar Reportes")
+    print("0. Salir")
+
+def gestionar_hoteles():
+    # TODO: Implementar la funcionalidad para gestionar hoteles
+    print("Funcionalidad para gestionar hoteles (pendiente de implementar)")
+
+def gestionar_clientes():
+    # TODO: Implementar la funcionalidad para gestionar clientes
+    print("Funcionalidad para gestionar clientes (pendiente de implementar)")
+
+def gestionar_reservas():
+    # TODO: Implementar la funcionalidad para gestionar reservas
+    print("Funcionalidad para gestionar reservas (pendiente de implementar)")
+
+def generar_reportes():
+    # TODO: Implementar la funcionalidad para generar reportes
+    print("Funcionalidad para generar reportes (pendiente de implementar)")
 
 def main():
-    print("Binevenido a SRG - Sistema de Registro de Hoteleria")
-
+    # Función principal que inicia la aplicación
+    print("Bienvenido a SRG - Sistema de Registro de Hoteleria")
+    
     menu()
     opcion = input("Seleccione una opcion: ")
-    
-    if opcion == "1":
-        registrar_hotel()
+    while True:
+        if opcion == '1':
+            gestionar_hoteles()
+        elif opcion == '2':
+            gestionar_clientes()
+        elif opcion == '3':
+            gestionar_reservas()
+        elif opcion == '4':
+            generar_reportes()
+        elif opcion == '0':
+            print("Saliendo del sistema...")
+            break
+        else:
+            print("Opcion no valida. Intente de nuevo.")
+        
+        menu()
+        opcion = input("Seleccione una opcion: ")
 
 if __name__ == "__main__":
     main()
