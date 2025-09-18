9eb6b6cb4b3f428196e2311f491fa7ba916f70d6
Ema <grossiemanuel71@gmail.com>
2025-09-18 20:35:38 -0300

    Funcion para gestionar hoteles Se agrego una funcion para agregar, consultar y eliminar hoteles

--- src/main.py
+++ src/main.py
@@ -74,8 +74,48 @@ def validar_fecha(fecha: str) -> bool:
     return True
 
 def gestionar_hoteles():
-    # TODO: Implementar la funcionalidad para gestionar hoteles
-    print("Funcionalidad para gestionar hoteles (pendiente de implementar)")
+    while True:
+        print("\n--- Gestión de Hoteles ---")
+        print("1. Agregar hotel")
+        print("2. Consultar hoteles")
+        print("3. Eliminar hotel")
+        print("4. Salir al menu principal")
+
+        opcion = int(input("Ingrese la opcion que quiere llevar a cabo: "))
+        if opcion == 1:
+            agregar_hotel()
+        elif opcion == 2:
+            consultar_hotel()
+        elif opcion == 3:
+            eliminar_hotel()
+        elif opcion == 4:
+            break
+        else:
+            print("Valor invalido")
+
+
+
+def agregar_hotel():
+    hotel = input("Ingrese el nombre del hotel: ")
+    localidad = input("Ingrese la ciudad donde se encuentra el hotel: ")
+    habitaciones = int(input("Ingrese la cantidad de habitaciones que tiene el hotel: "))
+    id_hotel = len(hoteles) + 1
+    hoteles.append({"ID":id_hotel,"Nombre":hotel,"Ubicacion":localidad,"Cantidad de Habitaciones":habitaciones})
+    print("Hotel agregado correctamente")
+    
+def consultar_hotel():
+    for hotel in hoteles:
+        if len(hotel) > 0:
+            print(hotel)
+        else:
+            print("No hay hoteles registrados")
+
+def eliminar_hotel():
+    borrar = input("Ingrese el hotel a eliminar: ")
+    for hotel in hoteles:
+        if borrar == hotel["Nombre"]:
+            hoteles.remove(hotel)
+            print("Hotel eliminado exitosamente")
 
 def gestionar_clientes():
     # TODO: Implementar la funcionalidad para gestionar clientes
