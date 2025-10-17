8796ecb07c569d7d6fa82ace1beac4d89edece99
Lautiim <lasalto05@gmail.com>
2025-10-17 13:27:46 -0300

    Modificacion del texto de  __init__

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,5 @@
 """
-Este archivo puede estar vacío.
-Su objetivo es convertir al directorio 'src' en un paquete de Python,
+El objetivo de este archivo es convertir al directorio 'src' en un paquete de Python,
 esto nos permite importar los otros archivos (como datos.py, utils.py, etc.)
 de forma más ordenada.
 """
