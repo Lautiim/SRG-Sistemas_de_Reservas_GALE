989db59045d10a767beebc6d2532e64d0b2e7ed5
Lautiim <lasalto05@gmail.com>
2025-10-16 22:44:21 -0300

    Configuración inicial del paquete src
    Se relleno el archivo __init__.py.
    Esto define a src/ como un paquete de Python, permitiendo importar los modulos para organizar mejor el proyecto.

---
+++ src/__init__.py
@@ -0,0 +1,6 @@
+"""
+Este archivo puede estar vacío.
+Su objetivo es convertir al directorio 'src' en un paquete de Python,
+esto nos permite importar los otros archivos (como datos.py, utils.py, etc.)
+de forma más ordenada.
+"""
